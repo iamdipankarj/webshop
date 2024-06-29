3691184f47bbbc08b321c11e73c6fe5874bc8384
Dipankar Jana <dipankar@okcredit.in>
2024-06-29 17:05:32 +0530

    Add current user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   # before_action :authenticate_user!
+  before_action :set_current_user, if: :user_signed_in?
+
+  def set_current_user
+    @email = current_user.email
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ Rails.application.routes.draw do
     :omniauth_callbacks => "users/omniauth_callbacks"
   }
 
-  root 'main#home'
+  root 'main#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
